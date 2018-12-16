d483ebbdfdfb30ab1e3fe969e5764b72b3dea09c
mohamedali1995 <muhamedali166@gmail.com>
2018-12-17 00:10:09 +0200

    Adding boost.

---
+++ testbench/testboost.cpp
@@ -0,0 +1,17 @@
+//
+// Created by syrix on 12/16/18.
+//
+#include<iostream>
+#include<string>
+
+#define BOOST_TEST_MODULE ADDITION_TEST
+#include<boost/test/unit_test.hpp>
+
+int add( int i, int j ) { return i+j; }
+
+BOOST_AUTO_TEST_CASE(addition_test) {
+
+    BOOST_CHECK(add(2,3)==5);
+
+}
+
