efd2f213edafe7c7f08dee98e2a54b440d5e10ab
Dawod <mhm.dawod@gmail.com>
2018-12-18 09:40:14 +0200

    Demo1 is almost finished

--- testbench/demo1.cpp
+++ testbench/demo1.cpp
@@ -3,17 +3,46 @@
 //
 #include<iostream>
 #include<string>
+#include "Simulator.h"
+using namespace std;
 
+//NOTE: boost generates the main for you if you use the BOOST_TEST_DYN_LINK , so we don't need to write main()
 #define BOOST_TEST_DYN_LINK
 
-#define BOOST_TEST_MODULE ADDITION_TEST
+#define BOOST_TEST_MODULE NETLIST
 #include<boost/test/unit_test.hpp>
 
-int add( int i, int j ) { return i+j; }
 
-BOOST_AUTO_TEST_CASE(addition_test) {
 
-    BOOST_CHECK(add(2,3)==5);
-
+BOOST_AUTO_TEST_CASE(Netlist_test1) {
+  string netlistFile = "testbench/input1.txt";
+  Simulator sim(netlistFile);
+  BOOST_CHECK(sim.netlist.getNodesCount()==2);
+  BOOST_CHECK(sim.netlist.getVoltageSourceCount()==1);
+  BOOST_CHECK(sim.netlist.hasActiveComponents==true);
 }
 
+BOOST_AUTO_TEST_CASE(Netlist_test2) {
+  string netlistFile = "testbench/input2.txt";
+  Simulator sim(netlistFile);
+  BOOST_CHECK(sim.netlist.getNodesCount()==2);
+  BOOST_CHECK(sim.netlist.getVoltageSourceCount()==1);
+  BOOST_CHECK(sim.netlist.hasActiveComponents==false);
+}
+
+BOOST_AUTO_TEST_CASE(Netlist_test3) {
+  string netlistFile = "testbench/input3.txt";
+  Simulator sim(netlistFile);
+  BOOST_CHECK(sim.netlist.getNodesCount()==5);
+  BOOST_CHECK(sim.netlist.getVoltageSourceCount()==2);
+  BOOST_CHECK(sim.netlist.hasActiveComponents==true);
+}
+
+
+BOOST_AUTO_TEST_CASE(Netlist_test4) {
+  string netlistFile = "testbench/input4.txt";
+  Simulator sim(netlistFile);
+  BOOST_CHECK(sim.netlist.getNodesCount()==13);
+  BOOST_CHECK(sim.netlist.getVoltageSourceCount()==5);
+  BOOST_CHECK(sim.netlist.hasActiveComponents==true);
+}
